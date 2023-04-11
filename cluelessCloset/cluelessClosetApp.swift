e4b67bade5622f4f70457dc5c3b72baf3edf602e
Srishti Kamra <srishtikamra07@gmail.com>
2023-04-11 09:29:14 -0500

    Initial Commit

---
+++ cluelessCloset/cluelessClosetApp.swift
@@ -0,0 +1,17 @@
+//
+//  cluelessClosetApp.swift
+//  cluelessCloset
+//
+//  Created by Srishti Kamra  on 4/11/23.
+//
+
+import SwiftUI
+
+@main
+struct cluelessClosetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
